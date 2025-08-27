2a2cb1f6820083080c08194d5c6923f5b63c8d30
Babin-exe <nepalbabin4@gmail.com>
2025-08-27 21:29:47 +0545

    Made change in the api

--- FoodApp/src/utils/constants.jsx
+++ FoodApp/src/utils/constants.jsx
@@ -1,15 +1,21 @@
 export const restaurantList = async () => {
+  // const data = await fetch(
+  //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9628669&lng=77.57750899999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  // );
   const data = await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9628669&lng=77.57750899999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9628669&lng=77.57750899999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
   );
+
   const json = await data.json();
   const restaurant =
     json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
       ?.restaurants || [];
 
-
   return restaurant;
 };
 
+// export const MENU_API =
+//   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9628669&lng=77.57750899999999&restaurantId=";
+
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9628669&lng=77.57750899999999&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9628669&lng=77.57750899999999&restaurantId=";
